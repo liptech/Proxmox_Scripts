15fb1946f9a18a8de09c405637f82b8e17874d51
Felipe Mendes <80398434+liptech@users.noreply.github.com>
2024-06-03 18:25:37 -0300

    Update quorum2.sh
    Removed Script comments

--- quorum2.sh
+++ quorum2.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 #while true; do
-    # Executa o comando pvecm status e armazena a saída em uma variável
+    
     output=$(pvecm status)
 
-    # Extrai o valor de Quorum da saída
     quorum=$(echo "$output" | grep "Nodes:" | awk '{print $2}')
 
-    # Verifica se o Quorum é menor que 2
     if [ "$quorum" -lt 2 ]; then
-        # Executa o comando pvecm expected 1
         pvecm expected 1
-        echo "$(date): Comando pvecm expected 1 executado." >> /var/log/syslog
+        echo "$(date): pvecm expected 1 command executed." >> /var/log/syslog
 #    else
 #        echo "$(date): Quorum é igual ou maior que 2. Nada a fazer." >> /var/log/syslog
     fi
